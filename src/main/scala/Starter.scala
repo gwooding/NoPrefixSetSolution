232c61b7a2c3e9e89c9e1a9e965fabb9392b6532
georgewooding <georgewooding@hotmail.co.uk>
2018-06-21 07:14:43 +0100

    code cleanup

--- src/main/scala/Starter.scala
+++ src/main/scala/Starter.scala
@@ -2,58 +2,56 @@ object Solution {
 
   def main(args: Array[String]) {
     try {
-      val sc = new java.util.Scanner(System.in);
-      var n = sc.nextInt();
-      var c = new Array[String](n)
+      val sc = new java.util.Scanner(System.in)
+      val n = sc.nextInt()
+      val c = new Array[String](n)
 
-      var root = BranchNode2(Map.empty[Char, MyNode2])
+      var root = BranchNode(Map.empty[Char, MyNode])
 
       for (c_i <- 0 to n) {
         if (c_i != 0) {
           c(c_i - 1) = sc.nextLine()
-          root = Tree2.fctn(c(c_i - 1), root, c(c_i - 1))
+          root = Tree.fctn(c(c_i - 1), root, c(c_i - 1))
         } else sc.nextLine()
       }
       println("GOOD SET")
     } catch {
-      case e: Exception => {
+      case e: Exception =>
         println("BAD SET")
         println(e.getMessage())
-      }
     }
   }
 }
 
-trait MyNode2
+trait MyNode
 
-case class BranchNode2(leaves: Map[Char, MyNode2]) extends MyNode2
+case class BranchNode(leaves: Map[Char, MyNode]) extends MyNode
 
-case class LeafNode2() extends MyNode2
+case class LeafNode() extends MyNode
 
-object Tree2 {
+object Tree {
 
-  def fctn(tail: String, bn: BranchNode2, fs: String): BranchNode2 = {
+  def fctn(tail: String, bn: BranchNode, fs: String): BranchNode = {
     val key = tail.charAt(0)
     if (bn.leaves.keySet.contains(key)) {
       if (tail.length() == 1) {
-        throw new Exception(fs);
+        throw new Exception(fs)
       } else {
         bn.leaves(key) match {
-          case a: LeafNode2 => throw new Exception(fs)
-          case b: BranchNode2 => {
+          case _: LeafNode => throw new Exception(fs)
+          case b: BranchNode =>
             val res = bn.leaves - key
             val final_res = res + (key -> fctn(tail.substring(1), b, fs))
-            BranchNode2(final_res)
-          }
+            BranchNode(final_res)
         }
       }
     } else {
       if (tail.length() == 1) {
-        val res: Map[Char, MyNode2] = bn.leaves
-        BranchNode2(res + (key -> LeafNode2()))
+        val res: Map[Char, MyNode] = bn.leaves
+        BranchNode(res + (key -> LeafNode()))
       } else {
-        val res = bn.leaves + (key -> fctn(tail.substring(1), BranchNode2(Map.empty[Char, MyNode2]), fs))
-        BranchNode2(res)
+        val res = bn.leaves + (key -> fctn(tail.substring(1), BranchNode(Map.empty[Char, MyNode]), fs))
+        BranchNode(res)
       }
     }
   }
